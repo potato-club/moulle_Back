d3ccbbeb721212ba71e30a6b25350d87fac9faca
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-07-04 19:02:19 +0900

    초기 설정

---
+++ src/test/java/com/gamza/moulle/MoulleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamza.moulle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoulleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
