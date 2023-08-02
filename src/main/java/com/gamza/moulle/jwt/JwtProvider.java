ce1c1006b8923f6c96d59d61f48e6812837d714b
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-08-02 20:19:07 +0900

    jwt 오류해결

--- src/main/java/com/gamza/moulle/jwt/JwtProvider.java
+++ src/main/java/com/gamza/moulle/jwt/JwtProvider.java
@@ -49,20 +49,19 @@ public class JwtProvider {
         Claims claims = Jwts.claims().setSubject(email);
         claims.put("roles",userEntity.getUserRole().toString());
 
-        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
         Date date = new Date();
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(date)
                 .setExpiration(new Date(date.getTime()+tokenTime))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     public String resolveAT(HttpServletRequest request) {
-        if (request.getHeader("authorization") != null){
-            return request.getHeader("authorization").substring(7);
+        if (request.getHeader("Authorization") != null){
+            return request.getHeader("Authorization").substring(7);
         }
         return null;
     }
@@ -75,13 +74,8 @@ public class JwtProvider {
 
     public boolean validateToken(String token) {
         try {
-            Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
-            Jws<Claims> claims = Jwts.parserBuilder()
-                    .setSigningKey(key)
-                    .build()
-                    .parseClaimsJws(token);
-
-            return !claims.getBody().getExpiration().before(new Date());
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
         } catch (MalformedJwtException e) {
             throw new MalformedJwtException("Invalid JWT token", e);
         } catch (ExpiredJwtException e) {
@@ -96,7 +90,7 @@ public class JwtProvider {
     }
 
     public void setHeaderAT(HttpServletResponse response, String AT) {
-        response.setHeader("authorization","Bearer "+AT);
+        response.setHeader("Authorization","Bearer "+AT);
     }
     public void setHeaderRT(HttpServletResponse response, String RT) {
         response.setHeader("refreshToken","Bearer "+RT);
@@ -107,11 +101,9 @@ public class JwtProvider {
         return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
     }
     public String getUserEmail(String token) {
-        JwtParser jwtParser = Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
-                .build();
+        String email = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
 
-        return jwtParser.parseClaimsJws(token).getBody().getSubject();
+        return email;
     }
 
 }
